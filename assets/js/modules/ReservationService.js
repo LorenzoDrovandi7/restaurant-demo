e63cafd774a9dfac517cf6c48cc1201869eb1d3b
LorenzoDrovandi7 <lorenzodrovandi17@gmail.com>
2025-09-09 10:15:30 -0300

    Update ReservationService.js

--- assets/js/modules/ReservationService.js
+++ assets/js/modules/ReservationService.js
@@ -1,5 +1,5 @@
 export async function sendReservation(data) {
-  const res = await fetch("http://localhost:3000/reserve", {
+  const res = await fetch("http://localhost:3000/reservations", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
